a8af92a8a319c2e371f901c84039848cac451608
tphorbin <tphorbin@gmail.com>
2017-10-30 18:26:51 -0500

    Updated for Windows commands

--- docker.py
+++ docker.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import subprocess as s
-import shlex
-import sys
+import sys, os, shlex
 
 # Set user variables for Docker Registry
 u = "legalaidfinder"
@@ -15,12 +14,15 @@ s.call(["docker-compose", "build"])
 s.call(["docker", "tag", "legalaidfinder/laf:latest", tag])
 
 # Docker login to allow push to registry
-p1 = s.Popen(shlex.split("cat docker.txt"), stdout=s.PIPE)
+if os.name == 'nt':
+    p1 = s.Popen(shlex.split("type docker.txt"), stdout=s.PIPE, shell=True)
+else:
+    p1 = s.Popen(shlex.split("cat docker.txt"), stdout=s.PIPE)
 p2 = s.Popen(shlex.split(docker_login), stdin=p1.stdout, stdout=s.PIPE, stderr=s.PIPE)
 p1.stdout.close()
 output = p2.communicate()[0]
-print output
+print(output)
 
 # Push image to Docker hub registry
 s.call(["docker", "push", tag])
-print "\nImage " + tag + " pushed to legalaidfinder/laf @ https://hub.docker.com/r/legalaidfinder/laf \n"
+print("\nImage " + tag + " pushed to legalaidfinder/laf @ https://hub.docker.com/r/legalaidfinder/laf \n")

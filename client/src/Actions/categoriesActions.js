c1fa9858b6da9201564a2820a8a66bf3cbf6b49d
AD\hermanb <hermanb@msoe.edu>
2018-02-06 22:27:43 -0600

    Added correct action

--- client/src/Actions/categoriesActions.js
+++ client/src/Actions/categoriesActions.js
@@ -1,12 +1,14 @@
-import Client, { CATEGORIES_ENDPOINT } from '../Client';
-import { FETCH_CATEGORIES_FULFILLED,
-         FETCH_CATEGORIES_REJECTED,
-         FETCH_CATEGORIES_DROPDOWN_FULFILLED,
-         REQUEST_REJECTED,
-         UPDATE_CATEGORY,
-         FETCH_CATEGORIES_FULL_DROPDOWN_FULFILLED,
-         ADD_CATEGORY,
-         DELETE_CATEGORY} from '../Reducers/categoriesReducer';
+import Client, { ADD_AGENCY_TO_CATEGORY, CATEGORIES_ENDPOINT } from '../Client';
+import {
+  FETCH_CATEGORIES_FULFILLED,
+  FETCH_CATEGORIES_REJECTED,
+  FETCH_CATEGORIES_DROPDOWN_FULFILLED,
+  REQUEST_REJECTED,
+  UPDATE_CATEGORY,
+  FETCH_CATEGORIES_FULL_DROPDOWN_FULFILLED,
+  ADD_CATEGORY,
+  DELETE_CATEGORY, ADD_AGENCY_TO_CATEGORY_SUCCESS,
+} from '../Reducers/categoriesReducer';
 
 export function fetchCategories() {
   return dispatch => Client.getRequest(CATEGORIES_ENDPOINT)
@@ -62,6 +64,16 @@ export function deleteCategories(id) {
   return dispatch => Client.deleteRequest(CATEGORIES_ENDPOINT, id)
     .then((response) => {
       dispatch({ type: DELETE_CATEGORY, payload: id });
+    })
+    .catch((err) => {
+      dispatch({ type: REQUEST_REJECTED, payload: err });
+    });
+}
+
+export function addAgencyToCategories(agencyId, categoryId, pushAgency) {
+  return dispatch => Client.postRequest(ADD_AGENCY_TO_CATEGORY, { agencyId, categoryId, pushAgency })
+    .then((response) => {
+      dispatch({ type: ADD_AGENCY_TO_CATEGORY_SUCCESS, payload: response });
     })
     .catch((err) => {
       dispatch({ type: REQUEST_REJECTED, payload: err });

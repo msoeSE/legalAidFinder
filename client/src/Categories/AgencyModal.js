89cbcb9248aa07ef34f02a712cc8bdf1bc4588c9
Bryant Holland <hollandbw@msoe.edu>
2018-03-21 23:57:24 -0500

    Added content to home page

--- client/src/Categories/AgencyModal.js
+++ client/src/Categories/AgencyModal.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import { Button, Modal } from 'semantic-ui-react';
+import { Button, Modal, Icon } from 'semantic-ui-react';
 
 class AgencyModal extends Component {
   render() {
-    const allEmails = this.props.agency.emails.length;
-
-
     if (!this.props.showModal) {
       return null;
     }
-
     return (
       <div>
         <Modal trigger={<Button className='small ui blue button'>Click here to learn more about this agency!</Button>} closeIcon>
@@ -19,16 +15,18 @@ class AgencyModal extends Component {
           <Modal.Content>
             <Modal.Description>
               <h2> Phone Number: </h2>
-              {this.props.agency.phone}
-              <h2>Email: </h2>
+              <div>
+                {this.props.agency.phone > 0 ? <div className='phoneNum'>{this.props.agency.phone} </div> : <div className='noPhone'>There is not a phone number on file for this agency</div>}
+              </div>
+              <h2>Email/s: </h2>
               {this.props.agency.emails.length > 0 ? this.props.agency.emails.map(email =>
-                <div>{email}</div>) : 'There are no emails on file for this agency'}
+                <div>{email}</div>) : <div className='noPhone'> There are no emails on file for this agency </div>}
               <h3> Hours of Operation: </h3>
             </Modal.Description>
           </Modal.Content>
           <Modal.Actions>
-            <Button color='green' onClick={this.handleClose} inverted>
-              <Icon name='checkmark' /> Got it
+            <Button className='ui blue button' href={this.props.agency.url}>
+              Visit Website
             </Button>
           </Modal.Actions>
         </Modal>
--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import {
+  Link,
+} from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
+
 
 
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className='homePage'>
+        <h1 className='homePageTitle'> Welcome to Wisconsin Civil Legal Aid! </h1>
+        <div>
+          Do you have a legal issue, but can’t afford to hire an attorney? Then you came to the right place!
+          <br />
+          <br />
+          Wisconsin Civil Legal Aid helps provide you with the information you need in order to request legal aid.  All you have to do is identify
+          your legal issue, and we will output a list of agencies that support your issue.  From there, you may contact the agency and receive the
+          proper assistance you need.
+          <br />
+          <br />
+          To get started, click the button below!
+          <br />
+          <br />
+          <Link to='/workflow'>
+            <Button className='large ui blue button'>Legal Aid Finder</Button>
+          </Link>
+        </div>
       </div>
     );
   }

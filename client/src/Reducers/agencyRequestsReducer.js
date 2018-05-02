5ac729bf3710993e2162e6559fe5e2e642a41e27
Alex Muench <muencha@msoe.edu>
2018-05-02 16:15:16 -0500

    Added update agency request functionality and added new attribute's to the agency request form's page

--- client/src/Reducers/agencyRequestsReducer.js
+++ client/src/Reducers/agencyRequestsReducer.js
@@ -1,8 +1,11 @@
+import { UPDATE_AGENCY } from './agenciesReducer'
+
 export const FETCH_AGENCY_REQUESTS = 'FETCH_AGENCY_REQUESTS';
 export const REQUEST_REJECTED = 'REQUEST_REJECTED';
 export const FETCH_AGENCY_REQUESTS_FULFILLED = 'FETCH_AGENCY_REQUESTS_FULFILLED';
 export const ADD_AGENCY_REQUEST = 'ADD_AGENCY_REQUEST';
 export const DELETE_AGENCY_REQUEST = 'DELETE_AGENCY_REQUEST';
+export const UPDATE_AGENCY_REQUEST = 'UPDATE_AGENCY_REQUEST';
 
 export default function reducer(state = {
   requests: [],
@@ -23,6 +26,14 @@ export default function reducer(state = {
     }
     case DELETE_AGENCY_REQUEST: {
       return { ...state, requests: state.requests.filter(req => req._id !== action.payload), error: null };
+    }
+    case UPDATE_AGENCY_REQUEST: {
+      const id = action.payload._id;
+      const newAgencyRequests = [ ...state.requests ];
+      const agencyRequestToUpdate = newAgencyRequests.findIndex(({ _id }) => _id === id);
+      newAgencyRequests[agencyRequestToUpdate] = action.payload;
+
+      return { ...state, requests: newAgencyRequests, error: null };
     }
   }
 
